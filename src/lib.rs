4ccf394800f2569b0258cc6821765726621dd75e
FoxowDV <65514843+FoxowDV@users.noreply.github.com>
2025-12-11 05:30:35 -0700

    udpate a lot

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,4 @@ pub use lexing::lexer::{lex_program, calculate_position};
 pub use lexing::token::{Token, TokenWithPosition, Position};
 
 pub use parser::parser::KFParser;
+pub use parser::parser::{parse_program};
